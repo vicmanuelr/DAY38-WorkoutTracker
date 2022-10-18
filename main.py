92bc2909184f6a4dbb49f67da1a0fb6991cbb138
Victor Ponce <vicmanuelr@gmail.com>
2022-10-17 18:48:45 -0600

    Added sheety api for storing data entry from nutritionix api
    - used datetime and date modules to store timestamp of entries
    - stored variables for authentication and sheet endpoint as enviromental
      variables
    - if multiple exercises especified in the entry used for loop to store
      multiple entries

--- main.py
+++ main.py
@@ -1,22 +1,51 @@
 import requests
 import os
-import datetime as dt
+from datetime import datetime, date, time
 
-NUTRI_APP_ID = os.environ.get("APP_ID")
-NUTRI_API_KEY = os.environ.get("API_KEY")
-NUTRI_HEADERS = {
-    "x-app-id": NUTRI_APP_ID,
-    "x-app-key": NUTRI_API_KEY
-}
-NUTRI_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
-NUTRI_QUERY = {
- "query": input("Tell me which exercises you did: "),
- "gender": "male",
- "weight_kg": 72.5,
- "height_cm": 173,
- "age": 35
-}
-response = requests.post(url=NUTRI_ENDPOINT, headers=NUTRI_HEADERS, json=NUTRI_QUERY)
-response.raise_for_status()
-data = response.json()
-print(data)
+
+# -------------------------------------- NUTRITIONIX API ------------------------------------------------ #
+
+def get_exercise() -> list:
+    NUTRI_APP_ID = os.environ.get("APP_ID")
+    NUTRI_API_KEY = os.environ.get("API_KEY")
+    NUTRI_HEADERS = {
+        "x-app-id": NUTRI_APP_ID,
+        "x-app-key": NUTRI_API_KEY
+    }
+    NUTRI_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+    NUTRI_QUERY = {
+     "query": input("Tell me which exercises you did: "),
+     "gender": "male",
+     "weight_kg": 72.5,
+     "height_cm": 173,
+     "age": 35
+    }
+    response = requests.post(url=NUTRI_ENDPOINT, headers=NUTRI_HEADERS, json=NUTRI_QUERY)
+    response.raise_for_status()
+    data = response.json()
+    exercises_list = data['exercises']
+    return exercises_list
+
+
+# -------------------------------------------- SH+EETY API --------------------------------------------#
+def new_entry(exercises: list):
+    auth_header = os.environ.get("AUTH_HEADER")
+    headers = {
+        "Authorization": auth_header
+    }
+    sheet_endpoint = os.environ.get("SHEETY_ENDPOINT")
+    for exercise in exercises:
+        entry = {
+            "workout": {
+                "date": date.today().strftime("%d/%m/%Y"),
+                "time": datetime.now().time().isoformat(timespec='seconds'),
+                "exercise": exercise["name"].title(),
+                "duration": exercise["duration_min"],
+                "calories": exercise["nf_calories"]
+            }
+        }
+        response = requests.post(headers=headers, url=sheet_endpoint, json=entry)
+        print(response.text)
+
+
+new_entry(get_exercise())
